09b77d8a82c7b8c59fd120a69e821bdf0ae49f45
Yoshua Wuyts <yoshuawuyts@gmail.com>
2018-06-23 12:12:31 +0200

    struct + quarter round

--- src/lib.rs
+++ src/lib.rs
@@ -3,6 +3,42 @@
 #![cfg_attr(feature = "nightly", doc(include = "../README.md"))]
 #![cfg_attr(test, deny(warnings))]
 
+pub struct ChaCha {
+  state: Vec<u32>,
+}
+
+impl ChaCha {
+  /// Create a new instance.
+  pub fn with_state(state: Vec<u32>) -> Self {
+    Self { state }
+  }
+
+  /// Access the internal state
+  #[inline]
+  pub fn state (&self) -> &[u32] {
+    &self.state
+  }
+
+  pub fn quarter_round(&mut self, a: u16, b: u16, c: u16, d: u16) {
+    let a = a as usize;
+    let b = b as usize;
+    let c = c as usize;
+    let d = d as usize;
+
+    let (a_res, b_res, c_res, d_res) = quarter_round(
+      self.state[a],
+      self.state[b],
+      self.state[c],
+      self.state[d],
+    );
+
+    self.state[a] = a_res;
+    self.state[b] = b_res;
+    self.state[c] = c_res;
+    self.state[d] = d_res;
+  }
+}
+
 #[inline]
 pub fn quarter_round(
   mut a: u32,
--- tests/test.rs
+++ tests/test.rs
@@ -1,5 +1,7 @@
 extern crate chacha;
 
+use chacha::ChaCha;
+
 #[test]
 fn quarter_round_test_vector() {
   let a = 0x11111111;
@@ -13,4 +15,25 @@ fn quarter_round_test_vector() {
   assert_eq!(b, 0xcb1cf8ce);
   assert_eq!(c, 0x4581472e);
   assert_eq!(d, 0x5881c4bb);
+}
+
+#[test]
+fn quarter_round_chacha() {
+  let state = [
+    0x879531e0, 0xc5ecf37d, 0x516461b1, 0xc9a62f8a,
+    0x44c20ef3, 0x3390af7f, 0xd9fc690b, 0x2a5f714c,
+    0x53372767, 0xb00a5631, 0x974c541a, 0x359e9963,
+    0x5c971061, 0x3d631689, 0x2098d9d6, 0x91dbd320,
+  ];
+
+  let mut inst = ChaCha::with_state(state.to_vec());
+  inst.quarter_round(2, 7, 8, 13);
+
+  let expected = [
+    0x879531e0, 0xc5ecf37d, 0xbdb886dc, 0xc9a62f8a,
+    0x44c20ef3, 0x3390af7f, 0xd9fc690b, 0xcfacafd2,
+    0xe46bea80, 0xb00a5631, 0x974c541a, 0x359e9963,
+    0x5c971061, 0xccc07c79, 0x2098d9d6, 0x91dbd320,
+  ];
+  assert_eq!(inst.state(), &expected);
 }
